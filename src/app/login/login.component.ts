619f7bf81688f4bbc7f2fbaf034736b6005e38f9
manucrosspay <manu.kp@crosspaymt.com>
2023-01-31 15:32:40 +0530

    ch

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -19,6 +19,7 @@ export class LoginComponent implements OnInit {
     1001:{acno:1001,username:"amal",password:"abc123",balance:0},
     1003:{acno:1003,username:"arun",password:"abc123",balance:0},
     1004:{acno:1004,username:"akhil",password:"abc123",balance:0},
+    1002:{acno:1002,username:"manu",password:"abc123",balance:0},
   }
 
   constructor() {}
