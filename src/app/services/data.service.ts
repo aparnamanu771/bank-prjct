4c188f62ffa4f2e3132fdd68dd21a40c805ca66d
manucrosspay <manu.kp@crosspaymt.com>
2023-02-09 11:17:07 +0530

    day10

--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -10,26 +10,63 @@ export class DataService {
 
   currentUser:any
   currentAcno:any 
+  userDetails:any
 
-  constructor() { }
+  constructor() {
+    this.getData()
+   }
 
 
-  userDetails:any={
-    1000:{acno:1000,username:"anu",password:"abc123",balance:0, transaction:[]},
-    1001:{acno:1001,username:"amal",password:"abc123",balance:0, transaction:[]},
-    1003:{acno:1003,username:"arun",password:"abc123",balance:0, transaction:[]},
-    1004:{acno:1004,username:"akhil",password:"abc123",balance:0, transaction:[]},
-    1002:{acno:1002,username:"manu",password:"abc123",balance:0, transaction:[]},
-  }
+  // userDetails:any={
+  //   1000:{ acno:1000, username:"anu",  password:"abc123", balance:0, transaction:[]},
+  //   1001:{ acno:1001, username:"amal", password:"abc123", balance:0, transaction:[]},
+  //   1003:{ acno:1003, username:"arun", password:"abc123", balance:0, transaction:[]},
+  //   1004:{ acno:1004, username:"akhil",password:"abc123", balance:0, transaction:[]},
+  //   1002:{ acno:1002, username:"manu", password:"abc123", balance:0, transaction:[]},
+  // }
+   
+  
+    saveData(){
+      if(this.userDetails){
+        localStorage.setItem("database",JSON.stringify(this.userDetails))
+      }
+      if(this.currentUser){
+        localStorage.setItem("currentUser",this.currentUser)
+      }
+      if(this.currentAcno){
+        localStorage.setItem("currentAcno",JSON.stringify(this.currentAcno))
+      }
+    }
+
+
+    getData(){
+
+      if(localStorage.getItem('database')){
+        this.userDetails=JSON.parse(localStorage.getItem('database') || "")
+      }
+
+      if(localStorage.getItem('currentUser')){
+        this.currentUser=localStorage.getItem('currentUser')
+      }
+
+      if(localStorage.getItem('currentAcno')){
+        this.currentAcno=JSON.parse(localStorage.getItem('currentAcno') ||"")
+      }
+
+    }
+
+
 
   register(uname:any,acno:any,psw:any){
     if(acno in this.userDetails){
       return false
     }
     else{
-      this.userDetails[acno]={acno,username:uname,password:psw,balance:0}
+      this.userDetails[acno]={acno, username:uname, password:psw, balance:0,transaction:[]}
       console.log(this.userDetails);
-      
+
+      this.saveData()
+
       return true
     }
   }
@@ -40,7 +77,10 @@ export class DataService {
       if(psw==userDetails[acno]["password"]){
       this.currentUser=userDetails[acno]["username"]
       // console.log(this.currentUser);
+
       this.currentAcno=acno
+
+      this.saveData()
       
        return true
       }
@@ -66,11 +106,15 @@ export class DataService {
 
         // update balance
         userDetails[acnum]["balance"]+=amnt
+
         // console.log(userDetails);
         
         // transaction data store
         userDetails[acnum]["transaction"].push({Type:"CREDIT",amount:amnt})
-        console.log(userDetails);
+        // console.log(userDetails);
+
+        this.saveData()
+
        // return current balance
         return userDetails[acnum]["balance"]
         }
@@ -101,7 +145,7 @@ export class DataService {
            // transaction data store
         userDetails[acnum]["transaction"].push({Type:"DEBIT",amount:amnt})
         
-        
+        this.saveData()
 
           // rturn curnt balance
           return userDetails[acnum]["balance"]
